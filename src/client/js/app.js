b6a964bd1a09894ec48cd8dbb4eb2bd15615d92e
DrewBCaldwell <87549282+DrewBCaldwell@users.noreply.github.com>
2021-10-12 22:06:21 -0500

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1074,7 +1074,7 @@ const socketInit = (() => {
     })();
     // The initialization function (this is returned)
     return port => {
-        let socket = new WebSocket('ws://' + window.location,hostname + ':' + port);
+        let socket = new WebSocket('ws://' + window.location.hostname + ':' + port);
         // Set up our socket
         socket.binaryType = 'arraybuffer';
         socket.open = false;
